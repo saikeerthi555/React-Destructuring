f25b448dbf8cdb8ceb5f368cc9543b25d81d9cbd
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-02-09 11:29:31 +0530

    Destructuring Arrays & Objects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+import animals from "./data.js";
+
+console.log(animals);
+
+//destructuring Arrays
+const [cat, dog] = animals;
+//console.log(cat);
+
+//destructuring a object
+// const {name, sound} = cat;
+// const { name: catName, sound: catSound } = cat;
+//const {name = "Fluffy", sound = "purr"} = cat;
+//destructuring a nested object then
+// const {
+//   name,
+//   sound,
+//   feedingRequirements: {food, water }
+// } = cat;
+
+// console.log(food);
+
 // CHALLENGE: uncomment the code below and see the car stats rendered
 // import React from "react";
 // import ReactDOM from "react-dom";
